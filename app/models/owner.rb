766b5b4e6cbedfe3decf151f400e0f0dbde4da7d
Joe King <j.king92@gmail.com>
2023-01-07 11:06:41 -0700

    Setup migrations and relationships with testing

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,6 @@
 class Owner < ApplicationRecord
   validates_presence_of :name
-
   has_many :machines
+  has_many :machine_snacks, through: :machines
+  has_many :snacks, through: :machine_snacks
 end
--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Owner, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should have_many :machines }
+    it { should have_many(:machine_snacks).through(:machines) }
+    it { should have_many(:snacks).through(:machine_snacks) }
   end
 end
